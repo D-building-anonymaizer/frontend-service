c77a2446aeaf365a385a8bdbc4fa9fdca90c0d2f
AkimaRea <140754658+AkimaRea@users.noreply.github.com>
2023-12-22 17:21:10 +0300

    clear useless code and add necessary deps

--- src/1_app/App.tsx
+++ src/1_app/App.tsx
@@ -1,11 +1,7 @@
 import { reatomContext } from "@reatom/npm-react";
-import {
-	createHashRouter
-} from "@vkontakte/vk-mini-apps-router";
 import { ctx } from "../6_shared/lib/reatom/ctx";
 
 
-const router = createHashRouter(routes);
 
 const App = (): JSX.Element => {
 	return (
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,11 @@
+import { connectLogger } from "@reatom/framework";
 import { createRoot } from "react-dom/client";
 import App from "./1_app/App";
-import { connectLogger } from "@reatom/framework";
 
-import bridge from "@vkontakte/vk-bridge";
-import "@vkontakte/vkui/dist/vkui.css";
 
-import "./6_shared/styles/styles.css";
 import { ctx } from "./6_shared/lib/reatom/ctx";
-import { fetchAuth } from "./6_shared/lib/reatom/actions/fetchAuth";
+import "./6_shared/styles/styles.css";
 
-bridge.send("VKWebAppInit").then((res) => res && fetchAuth(ctx));
 
 process.env.NODE_ENV === "development" && connectLogger(ctx);
 
