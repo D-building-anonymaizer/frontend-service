1f65ce000c5656e10fd7d83bdf1d3fec9784380f
AkimaRea <140754658+AkimaRea@users.noreply.github.com>
2023-12-23 05:14:59 +0300

    image declaration added

--- src/declaration.d.ts
+++ src/declaration.d.ts
@@ -1,29 +1,39 @@
 declare module "*.module.css" {
-	const classes: { [key: string]: string; };
+	const classes: { [key: string]: string };
 	export default classes;
 }
 
 declare module "*.module.scss" {
-	const classes: { [key: string]: string; };
+	const classes: { [key: string]: string };
 	export default classes;
 }
 
 declare module "*.module.sass" {
-	const classes: { [key: string]: string; };
+	const classes: { [key: string]: string };
 	export default classes;
 }
 
 declare module "*.module.less" {
-	const classes: { [key: string]: string; };
+	const classes: { [key: string]: string };
 	export default classes;
 }
 
 declare module "*.module.styl" {
-	const classes: { [key: string]: string; };
+	const classes: { [key: string]: string };
 	export default classes;
 }
 
 declare module "*.jpg" {
+	const value: any;
+	export = value;
+}
+
+declare module "*.png" {
+	const value: any;
+	export = value;
+}
+
+declare module "*.svg" {
 	const value: any;
 	export = value;
 }
