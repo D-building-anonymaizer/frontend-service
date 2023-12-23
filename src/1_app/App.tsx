350e2f657506a31ea6060e117cb05dd36ee49667
AkimaRea <140754658+AkimaRea@users.noreply.github.com>
2023-12-23 05:12:45 +0300

    setup router and code sanitizing

--- src/1_app/App.tsx
+++ src/1_app/App.tsx
@@ -1,35 +1,23 @@
 import { reatomContext } from "@reatom/npm-react";
-import { Route, Router, Routes } from "react-router";
+import { Route, Routes } from "react-router";
+import { BrowserRouter } from "react-router-dom";
 import { Page404 } from "../2_pages/404/ui/index";
 import { ctx } from "../6_shared/lib/reatom/ctx";
-import { useEffect } from "react";
-import { BrowserRouter } from "react-router-dom";
 import { Home } from "./../2_pages/home/ui/Home";
-import { hot } from "react-hot-loader";
+import { Layout } from "./../2_pages/layout/ui/Layout";
 
 const App = (): JSX.Element => {
 	return (
 		<reatomContext.Provider value={ctx}>
 			<BrowserRouter>
 				<Routes>
-					<Route path='/'>
+					<Route
+						path='/'
+						element={<Layout />}>
 						<Route
 							index
 							element={<Home />}
 						/>
-						{/* 
-					<Route
-						path='about'
-						element={<About />}
-					/>
-					<Route
-						path='dashboard'
-						element={<Dashboard />}
-					/> */}
-
-						{/* Using path="*"" means "match anything", so this route
-                acts like a catch-all for URLs that we don't have explicit
-                routes for. */}
 						<Route
 							path='*'
 							element={<Page404 />}
